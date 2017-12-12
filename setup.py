6f9b1a48c4792d0edf2d26b12d03415b2f6f1006
Olivier Cazade <olivier.cazade@gmail.com>
2017-12-12 17:07:36 +0100

    Added long description

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='poloniex_aio',
-    version='0.1.0b4',
+    version='0.1.0b5',
     url='https://github.com/OlivierCazade/poloniex_aio',
     author='Olivier Cazade',
     author_email='olivier.cazade@gmail.com',
     license='MIT',
     packages=find_packages(),
     description='An async library for Poloniex api',
+    long_description=long_description,
     python_requires='>=3.5'
 )
